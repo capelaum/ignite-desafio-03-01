482f37cf785395105376328d09002f7e86278e06
Luís Vinicius Capelletto <thecapellett@gmail.com>
2021-05-30 23:22:39 -0300

    fix Post tests

--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -1,5 +1,5 @@
 import { GetStaticPaths, GetStaticProps } from 'next';
-import { useRouter } from 'next/router'
+import { useRouter } from 'next/router';
 import Head from 'next/head';
 import { FiUser, FiCalendar, FiClock } from 'react-icons/fi';
 import Prismic from '@prismicio/client';
@@ -11,6 +11,7 @@ import { getPrismicClient } from '../../services/prismic';
 
 import commonStyles from '../../styles/common.module.scss';
 import styles from './post.module.scss';
+import { RichText } from 'prismic-dom';
 
 interface Post {
   first_publication_date: string | null;
@@ -35,23 +36,21 @@ interface PostProps {
 }
 
 export default function Post({ post }: PostProps) {
-
   const router = useRouter();
 
   if (router?.isFallback) {
-    return <h2>Carregando...</h2>
+    return <h2>Carregando...</h2>;
   }
 
   const totalWords = post.data.content.reduce((total, content) => {
+    const headingCount = content.heading.split(' ').length;
+    const textArray = content.body.map(body => body.text);
+    const bodyCount = textArray.join(' ').split(' ').length;
 
-    const headingCount = (content.heading).split(" ").length;
-    const textArray = content.body.map(body => body.text)
-    const bodyCount = textArray.join(' ').split(" ").length;
+    return total + (headingCount + bodyCount);
+  }, 0);
 
-    return total + (headingCount + bodyCount)
-  }, 0)
-
-  const timeToRead = Math.ceil(totalWords / 200)
+  const timeToRead = Math.ceil(totalWords / 200);
   // console.log("🚀 ~ timeToRead", timeToRead)
 
   return (
@@ -62,8 +61,8 @@ export default function Post({ post }: PostProps) {
 
       <div className={styles.bannerContainer}>
         <img
-          src={post?.data.banner.url}
-          alt={post?.data.banner.alt}
+          src={post.data.banner.url}
+          alt={post.data.title}
           className={styles.banner}
         />
       </div>
@@ -74,11 +73,13 @@ export default function Post({ post }: PostProps) {
           <div className={commonStyles.infoContainer}>
             <time>
               <FiCalendar />
-              {post?.first_publication_date}
+              {format(new Date(post.first_publication_date), 'dd MMM yyyy', {
+                locale: ptBR,
+              })}
             </time>
             <span>
               <FiUser />
-              {post?.data.author}
+              {post.data.author}
             </span>
             <span>
               <FiClock />
@@ -86,7 +87,7 @@ export default function Post({ post }: PostProps) {
             </span>
           </div>
 
-          {post?.data.content.map(content => (
+          {post.data.content.map(content => (
             <div key={content.heading} className={styles.postContent}>
               <h2>{content.heading}</h2>
               {content.body.map((body, index) => (
@@ -122,25 +123,28 @@ export const getStaticProps: GetStaticProps = async context => {
   const { slug } = context.params;
   const response = await prismic.getByUID('posts', String(slug), {});
 
+  console.log(JSON.stringify(response.data, null, 2));
+
+  // response.data.content.map(content => RichText.asText(content))
+
   const post = {
-    first_publication_date: format(
-      new Date(response.first_publication_date),
-      'dd MMM yyyy',
-      { locale: ptBR }
-    ),
+    uid: response.uid,
+    first_publication_date: response.first_publication_date,
     data: {
       title: response.data.title,
+      subtitle: response.data.subtitle,
       author: response.data.author,
       banner: {
         url: response.data.banner.url,
-        alt: response.data.banner.alt,
       },
       content: response.data.content.map(content => {
         return {
           heading: content.heading,
           body: content.body.map(body => {
             return {
-              text: body.type === 'paragraph' ? body.text : '',
+              text: body.text,
+              type: body.type,
+              spans: body.spans,
             };
           }),
         };
